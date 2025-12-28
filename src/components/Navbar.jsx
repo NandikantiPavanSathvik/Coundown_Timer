3e0024d94e7857af21c032dad4fbe349bae60a85
pavansathvik <pavansathviknandikanti@gmail.com>
2025-12-28 20:53:53 +0530

    removed comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-
-
 import { Link } from 'react-router-dom';
 import { Home, PlusCircle } from 'lucide-react';
 
--- src/hooks/useEventLogic.js
+++ src/hooks/useEventLogic.js
@@ -1,29 +1,4 @@
 
-
-// import { useState } from 'react';
-
-// export const useEventLogic = () => {
-//   const [events, setEvents] = useState([]);
-
-//   const addEvent = (newEvent) => {
-//     // Calculate the exact moment in the future this timer ends
-//     const targetDate = Date.now() + (newEvent.seconds * 1000);
-    
-//     setEvents((prev) => [...prev, { 
-//       id: Date.now(), 
-//       name: newEvent.name, 
-//       targetDate: targetDate 
-//     }]);
-//   };
-
-//   const deleteEvent = (id) => {
-//     setEvents((prev) => prev.filter(event => event.id !== id));
-//   };
-
-//   return { events, addEvent, deleteEvent };
-// };
-
-
 import { useState } from 'react';
 
 export const useEventLogic = () => {
